9a348dff6d56d0dd3d81f6a51af517a0a8b1f6b1
emsyaiful <jihad.amrulloh@gmail.com>
2016-10-17 18:59:10 +0700

    Tambah expired

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('mainApp', ['ngRoute'])
+var app = angular.module('mainApp', ['ngRoute','ngIdle'])
 
 app.config(['$routeProvider', function($routeProvider, $window){
 	$routeProvider
@@ -19,6 +19,16 @@ app.config(['$routeProvider', function($routeProvider, $window){
 		controller: 'register'
 	})
 }])
+
+app.config(['KeepaliveProvider', 'IdleProvider', function(KeepaliveProvider, IdleProvider) {
+	IdleProvider.idle(5);
+	IdleProvider.timeout(5);
+	KeepaliveProvider.interval(10);
+}]);
+
+app.run(function(Idle){
+	Idle.watch();
+})
 
 app.controller('mainPage', function($scope, $http, $timeout, $interval) {
 	$scope.flag = 0
@@ -55,7 +65,7 @@ app.controller('mainPage', function($scope, $http, $timeout, $interval) {
 	});
 	// $scope.reloadData()
 });
-app.controller('homePage', function($scope, $http, $timeout, $rootScope) {
+app.controller('homePage', function($scope, $http, $timeout, $rootScope, Idle) {
 	$scope.reloadData = function(){
 		$http.get('getAll.php', {}).success(function(data, status){
 			$scope.data = data
@@ -66,8 +76,36 @@ app.controller('homePage', function($scope, $http, $timeout, $rootScope) {
 			// console.log(data)
 		})
 	}
+
+	$scope.$on('IdleStart', function() {
+        console.log('idle start')
+    });
+
+    $scope.$on('IdleWarn', function(e, countdown) {
+        // follows after the IdleStart event, but includes a countdown until the user is considered timed out
+        // the countdown arg is the number of seconds remaining until then.
+        // you can change the title or display a warning dialog from here.
+        // you can let them resume their session by calling Idle.watch()
+        console.log('idle warn')
+    });
+
+    $scope.$on('IdleTimeout', function() {
+        // the user has timed out (meaning idleDuration + timeout has passed without any activity)
+        // this is where you'd log them
+        console.log('idle TO')
+    });
+
+    $scope.$on('IdleEnd', function() {
+        // the user has come back from AFK and is doing stuff. if you are warning them, you can use this to hide the dialog
+        console.log('idle End')
+    });
+
+    $scope.$on('Keepalive', function() {
+        // do something to keep the user's session alive
+    });
 	$scope.reloadData()
 });
+
 app.controller('nfcControl', function($scope, $http, $routeParams, $rootScope) {
 	var data = $routeParams.id
 	$http({
